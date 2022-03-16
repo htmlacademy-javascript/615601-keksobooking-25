71485f7ce8e4ddb3ec15b8c1878822462a5f9353
Keks <keks@htmlacademy.ru>
2022-03-16 18:28:49 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,22 +1,42 @@
-function random (min, max) {
-  if (max <= min) {
-    return "random error!"
-  } else {
-    return Math.floor(Math.random() * (max - min + 1) ) + min
-  }
+function isPositive (value) {
+  return Math.sign(value)
 }
 
-random(1, 8)
-
-function coord (min, max, round) {
-  if (max <= min) {
-    return "round error!"
-  } else {
+function getRandomInt (min, max) {
+  if (isPositive(min) !== 1) {
     min = Math.abs(min);
-    max = Math.abs(max);
-    const num = Math.random() * (max - min + 1) + min;
-    return num.toFixed(round)
   }
+  if (isPositive(max) !== 1) {
+    max = Math.abs(max);
+  }
+  // хочется как-то объединить два if с проверками на положительное число. Но как это сделать? Как понять, какое именно отрицательное число пришло, min или max, и какое нужно модифицировать? Может, просто в начале функции насильно их абсолютить?
+  if (min === max) {
+    return min
+  }
+  if (max < min) {
+    let maxNum = min;
+    let minNum = max;
+    return Math.floor(Math.random() * (maxNum - minNum + 1) ) + minNum
+  }
+  return Math.floor(Math.random() * (max - min + 1) ) + min
 }
 
-coord(-1.1, 8.22, 3)
+getRandomInt(1, 8);
+
+function getRandomNumber (min, max, precision) {
+  min = Math.abs(min);
+  max = Math.abs(max);
+  // примерно как здесь, я в самом начале избавляюсь от возможных отрицательных чисел
+  if (min === max) {
+    return min
+  }
+  if (max < min) {
+    let maxNum = min;
+    let minNum = max;
+    return Math.floor(Math.random() * (maxNum - minNum + 1) ) + minNum
+  }
+  const num = Math.random() * (max - min + 1) + min;
+  return +(num.toFixed(precision))
+}
+
+getRandomNumber(9, 1, 3);
