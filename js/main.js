52cc7c791c200b879f34a47b117a91b6a921173c
alicepepel <alicepepel3000@gmail.com>
2022-04-14 00:28:17 +0300

    some fixes in random functions

--- js/main.js
+++ js/main.js
@@ -1,42 +1,16 @@
-function isPositive (value) {
-  return Math.sign(value)
+function getRandomPositiveInt (a, b) {
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function getRandomInt (min, max) {
-  if (isPositive(min) !== 1) {
-    min = Math.abs(min);
-  }
-  if (isPositive(max) !== 1) {
-    max = Math.abs(max);
-  }
-  // хочется как-то объединить два if с проверками на положительное число. Но как это сделать? Как понять, какое именно отрицательное число пришло, min или max, и какое нужно модифицировать? Может, просто в начале функции насильно их абсолютить?
-  if (min === max) {
-    return min
-  }
-  if (max < min) {
-    let maxNum = min;
-    let minNum = max;
-    return Math.floor(Math.random() * (maxNum - minNum + 1) ) + minNum
-  }
-  return Math.floor(Math.random() * (max - min + 1) ) + min
-}
+getRandomPositiveInt(1, 8);
 
-getRandomInt(1, 8);
-
-function getRandomNumber (min, max, precision) {
-  min = Math.abs(min);
-  max = Math.abs(max);
-  // примерно как здесь, я в самом начале избавляюсь от возможных отрицательных чисел
-  if (min === max) {
-    return min
-  }
-  if (max < min) {
-    let maxNum = min;
-    let minNum = max;
-    return Math.floor(Math.random() * (maxNum - minNum + 1) ) + minNum
-  }
+function getRandomPositiveFloat (a, b, precision) {
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const num = Math.random() * (max - min + 1) + min;
-  return +(num.toFixed(precision))
+  return +num.toFixed(precision)
 }
 
-getRandomNumber(9, 1, 3);
+getRandomPositiveFloat(9, 1, 3);
