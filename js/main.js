772475004c9c14031b7e9ffabb11e33ba792e415
Alice Pepel <alicepepel3000@gmail.com>
2022-04-19 21:18:13 +0700

    add object generator

--- js/main.js
+++ js/main.js
@@ -1,42 +1,60 @@
-function isPositive (value) {
-  return Math.sign(value)
+function getRandomPositiveInt (a, b) {
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
+console.log(getRandomPositiveInt(1, 8))
 
-function getRandomInt (min, max) {
-  if (isPositive(min) !== 1) {
-    min = Math.abs(min);
-  }
-  if (isPositive(max) !== 1) {
-    max = Math.abs(max);
-  }
-  // хочется как-то объединить два if с проверками на положительное число. Но как это сделать? Как понять, какое именно отрицательное число пришло, min или max, и какое нужно модифицировать? Может, просто в начале функции насильно их абсолютить?
-  if (min === max) {
-    return min
-  }
-  if (max < min) {
-    let maxNum = min;
-    let minNum = max;
-    return Math.floor(Math.random() * (maxNum - minNum + 1) ) + minNum
-  }
-  return Math.floor(Math.random() * (max - min + 1) ) + min
-}
-
-getRandomInt(1, 8);
-
-function getRandomNumber (min, max, precision) {
-  min = Math.abs(min);
-  max = Math.abs(max);
-  // примерно как здесь, я в самом начале избавляюсь от возможных отрицательных чисел
-  if (min === max) {
-    return min
-  }
-  if (max < min) {
-    let maxNum = min;
-    let minNum = max;
-    return Math.floor(Math.random() * (maxNum - minNum + 1) ) + minNum
-  }
+function getRandomPositiveFloat (a, b, precision) {
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const num = Math.random() * (max - min + 1) + min;
-  return +(num.toFixed(precision))
+  return +num.toFixed(precision)
 }
+getRandomPositiveFloat(9, 1, 3);
 
-getRandomNumber(9, 1, 3);
+// Create offers objects
+
+const OFFER_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const OFFER_CHECKIN = ['12:00', '13:00', '14:00'];
+const OFFER_CHECKOUT = ['12:00', '13:00', '14:00'];
+const OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomPositiveInt(0, elements.length - 1)];
+};
+
+let id = 0;
+
+const createOffer = () => {
+  id++;
+  return {
+    author: {
+      avatar: 'img/avatars/user' + (id === 10 ? '' : '0') + id + '.png'
+    },
+    offer: {
+      title: 'Некое помещение',
+      address: '{{location.lat}}, {{location.lng}}',
+      price: getRandomPositiveInt(1, 100000),
+      type: getRandomArrayElement(OFFER_TYPE),
+      rooms: getRandomPositiveInt(1, 15),
+      guests: getRandomPositiveInt(1, 200),
+      checkin: getRandomArrayElement(OFFER_CHECKIN),
+      checkout: getRandomArrayElement(OFFER_CHECKOUT),
+      features: [
+
+      ],
+      description: 'Длинное, а возможно короткое или среднее описание помещения',
+      photos: [
+
+      ],
+      location: {
+
+      }
+    },
+  };
+};
+
+const similarWizards = Array.from({length: 10}, createOffer);
+
+console.log(similarWizards);
